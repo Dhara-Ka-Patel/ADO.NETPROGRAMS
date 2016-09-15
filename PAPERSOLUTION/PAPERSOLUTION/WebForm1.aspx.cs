27804fc5a22854a636964939fb2fca4cb0beec81
anjali-kandoriya <anjalipharsora29@gmail.com>
2016-09-15 12:46:17 +0530

    jhj

---
+++ PAPERSOLUTION/PAPERSOLUTION/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PAPERSOLUTION
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("<%$ ConnectionStrings:ce53ConnectionString %>");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [login]");
+            con.Open();
+            SqlDataReader rd = cmd.ExecuteReader();
+
+        }
+    }
+}
